100ac331f8a85f0219f38b5137861d56eea7e59f
KIMTAEHOON <xognsxo1491@naver.com>
2026-06-22 21:56:21 +0900

    feat: fx-notification-service 초안 생성

---
+++ fx-notification-service/src/main/kotlin/com/xooxz/notification/NotificationApplication.kt
@@ -0,0 +1,11 @@
+package com.xooxz.notification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FxNotificationServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<FxNotificationServiceApplication>(*args)
+}
---
+++ fx-notification-service/src/test/kotlin/com/xooxz/notification/FxNotificationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xooxz.notification
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FxNotificationServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
