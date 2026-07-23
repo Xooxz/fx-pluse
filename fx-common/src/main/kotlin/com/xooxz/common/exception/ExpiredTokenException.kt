1579f021a0b1b17eefdfc4db429dd733ed709d36
KIMTAEHOON <xognsxo1491@naver.com>
2026-07-23 22:15:43 +0900

    feat: [common] common 모듈 제거

--- fx-common/src/main/kotlin/com/xooxz/common/exception/ExpiredTokenException.kt
+++
@@ -1,8 +0,0 @@
-package com.xooxz.common.exception
-
-/**
- * Access Token의 유효기간이 만료된 경우 발생하는 예외
- */
-class ExpiredTokenException(
-    message: String = "만료된 토큰입니다."
-) : RuntimeException(message)
--- fx-common/src/main/kotlin/com/xooxz/common/exception/InvalidTokenException.kt
+++
@@ -1,8 +0,0 @@
-package com.xooxz.common.exception
-
-/**
- * JWT가 위조되었거나 형식이 올바르지 않은 경우 발생하는 예외
- */
-class InvalidTokenException(
-    message: String = "유효하지 않은 토큰입니다."
-) : RuntimeException(message)
